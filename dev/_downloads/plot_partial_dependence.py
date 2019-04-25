967c7d64f727d96a66a195df2e38ef288ed254b3
scikit-learn <olivier.grisel+sklearn-ci@gmail.com>
2019-04-25 20:11:37 +0000

    Pushing the docs to dev/ for branch: master, commit 37a9d187f9eb4864fb52556e9f293f8ff4f698b7

--- dev/_downloads/plot_partial_dependence.py
+++ dev/_downloads/plot_partial_dependence.py
@@ -8,35 +8,47 @@ and a set of 'target' features, marginalizing over the
 values of all other features (the complement features). Due to the limits
 of human perception the size of the target feature set must be small (usually,
 one or two) thus the target features are usually chosen among the most
-important features
-(see :attr:`~sklearn.ensemble.GradientBoostingRegressor.feature_importances_`).
+important features.
 
 This example shows how to obtain partial dependence plots from a
-:class:`~sklearn.ensemble.GradientBoostingRegressor` trained on the California
-housing dataset. The example is taken from [1]_.
+:class:`~sklearn.neural_network.MLPRegressor` and a
+:class:`~sklearn.ensemble.GradientBoostingRegressor` trained on the
+California housing dataset. The example is taken from [1]_.
 
-The plot shows four one-way and one two-way partial dependence plots.
-The target variables for the one-way PDP are:
-median income (`MedInc`), avg. occupants per household (`AvgOccup`),
-median house age (`HouseAge`), and avg. rooms per household (`AveRooms`).
+The plots show four 1-way and two 1-way partial dependence plots (ommitted for
+:class:`~sklearn.neural_network.MLPRegressor` due to computation time).
+The target variables for the one-way PDP are: median income (`MedInc`),
+average occupants per household (`AvgOccup`), median house age (`HouseAge`),
+and average rooms per household (`AveRooms`).
 
 We can clearly see that the median house price shows a linear relationship
 with the median income (top left) and that the house price drops when the
-avg. occupants per household increases (top middle).
+average occupants per household increases (top middle).
 The top right plot shows that the house age in a district does not have
 a strong influence on the (median) house price; so does the average rooms
 per household.
 The tick marks on the x-axis represent the deciles of the feature values
 in the training data.
 
+We also observe that :class:`~sklearn.neural_network.MLPRegressor` has much
+smoother predictions than
+:class:`~sklearn.ensemble.GradientBoostingRegressor`. For the plots to be
+comparable, it is necessary to subtract the average value of the target
+``y``: The 'recursion' method, used by default for
+:class:`~sklearn.ensemble.GradientBoostingRegressor`, does not account for
+the initial predictor (in our case the average target). Setting the target
+average to 0 avoids this bias.
+
 Partial dependence plots with two target features enable us to visualize
 interactions among them. The two-way partial dependence plot shows the
-dependence of median house price on joint values of house age and avg.
+dependence of median house price on joint values of house age and average
 occupants per household. We can clearly see an interaction between the
-two features:
-For an avg. occupancy greater than two, the house price is nearly independent
-of the house age, whereas for values less than two there is a strong dependence
-on age.
+two features: for an average occupancy greater than two, the house price is
+nearly independent of the house age, whereas for values less than two there
+is a strong dependence on age.
+
+On a third figure, we have plotted the same partial dependence plot, this time
+in 3 dimensions.
 
 .. [1] T. Hastie, R. Tibshirani and J. Friedman,
     "Elements of Statistical Learning Ed. 2", Springer, 2009.
@@ -48,51 +60,62 @@ print(__doc__)
 
 import numpy as np
 import matplotlib.pyplot as plt
-
 from mpl_toolkits.mplot3d import Axes3D
 
-from sklearn.model_selection import train_test_split
+from sklearn.inspection import partial_dependence
+from sklearn.inspection import plot_partial_dependence
 from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.ensemble.partial_dependence import plot_partial_dependence
-from sklearn.ensemble.partial_dependence import partial_dependence
+from sklearn.neural_network import MLPRegressor
 from sklearn.datasets.california_housing import fetch_california_housing
 
 
 def main():
     cal_housing = fetch_california_housing()
 
-    # split 80/20 train-test
-    X_train, X_test, y_train, y_test = train_test_split(cal_housing.data,
-                                                        cal_housing.target,
-                                                        test_size=0.2,
-                                                        random_state=1)
+    X, y = cal_housing.data, cal_housing.target
     names = cal_housing.feature_names
 
-    print("Training GBRT...")
-    clf = GradientBoostingRegressor(n_estimators=100, max_depth=4,
+    # Center target to avoid gradient boosting init bias: gradient boosting
+    # with the 'recursion' method does not account for the initial estimator
+    # (here the average target, by default)
+    y -= y.mean()
+
+    print("Training MLPRegressor...")
+    est = MLPRegressor(activation='logistic')
+    est.fit(X, y)
+    print('Computing partial dependence plots...')
+    # We don't compute the 2-way PDP (5, 1) here, because it is a lot slower
+    # with the brute method.
+    features = [0, 5, 1, 2]
+    plot_partial_dependence(est, X, features, feature_names=names,
+                            n_jobs=3, grid_resolution=50)
+    fig = plt.gcf()
+    fig.suptitle('Partial dependence of house value on non-location features\n'
+                 'for the California housing dataset, with MLPRegressor')
+    plt.subplots_adjust(top=0.9)  # tight_layout causes overlap with suptitle
+
+    print("Training GradientBoostingRegressor...")
+    est = GradientBoostingRegressor(n_estimators=100, max_depth=4,
                                     learning_rate=0.1, loss='huber',
                                     random_state=1)
-    clf.fit(X_train, y_train)
-    print(" done.")
-
-    print('Convenience plot with ``partial_dependence_plots``')
-
+    est.fit(X, y)
+    print('Computing partial dependence plots...')
     features = [0, 5, 1, 2, (5, 1)]
-    fig, axs = plot_partial_dependence(clf, X_train, features,
-                                       feature_names=names,
-                                       n_jobs=3, grid_resolution=50)
-    fig.suptitle('Partial dependence of house value on nonlocation features\n'
-                 'for the California housing dataset')
-    plt.subplots_adjust(top=0.9)  # tight_layout causes overlap with suptitle
+    plot_partial_dependence(est, X, features, feature_names=names,
+                            n_jobs=3, grid_resolution=50)
+    fig = plt.gcf()
+    fig.suptitle('Partial dependence of house value on non-location features\n'
+                 'for the California housing dataset, with Gradient Boosting')
+    plt.subplots_adjust(top=0.9)
 
     print('Custom 3d plot via ``partial_dependence``')
     fig = plt.figure()
 
     target_feature = (1, 5)
-    pdp, axes = partial_dependence(clf, target_feature,
-                                   X=X_train, grid_resolution=50)
+    pdp, axes = partial_dependence(est, X, target_feature,
+                                   grid_resolution=50)
     XX, YY = np.meshgrid(axes[0], axes[1])
-    Z = pdp[0].reshape(list(map(np.size, axes))).T
+    Z = pdp[0].T
     ax = Axes3D(fig)
     surf = ax.plot_surface(XX, YY, Z, rstride=1, cstride=1,
                            cmap=plt.cm.BuPu, edgecolor='k')
@@ -103,7 +126,7 @@ def main():
     ax.view_init(elev=22, azim=122)
     plt.colorbar(surf)
     plt.suptitle('Partial dependence of house value on median\n'
-                 'age and average occupancy')
+                 'age and average occupancy, with Gradient Boosting')
     plt.subplots_adjust(top=0.9)
 
     plt.show()
