a7a540a2db9816dd25591ba2f23405f1020ab128
Joel Nothman <joel.nothman@gmail.com>
2019-08-13 00:46:09 +1000

    Improvements to wording (some due to reviews)

--- versionwarning.js
+++ versionwarning.js
@@ -1,4 +1,4 @@
-(() => {
+(function() {
     var latestStable = '0.21';
     var goodPaths = ['stable', 'dev', latestStable];
     var showWarning = (msg) => {
@@ -16,16 +16,19 @@
     if (location.hostname == 'scikit-learn.org') {
         var versionPath = location.pathname.split('/')[1];
         if (!goodPaths.includes(versionPath)) {
-            showWarning('You are looking at documentation for an old release of ' +
+            showWarning('This is documentation for an old release of ' +
                         'Scikit-learn (version ' + versionPath + '). Try the ' +
-                        '<a href="https://scikit-learn.org">latest release</a> or ' +
+                        '<a href="https://scikit-learn.org">latest stable ' +
+                        'release</a> (version ' + latestStable + ') or ' +
                         '<a href="https://scikit-learn.org/dev">development</a> ' +
-                        'versions.')
+                        '(unstable) versions.')
         } else if (versionPath == 'dev') {
-            showWarning('You are looking at documentation for the unstable ' +
-                        'development version of Scikit-learn. See also the ' +
-                        '<a href="https://scikit-learn.org">latest release</a> ' +
-                        'version.')
+            showWarning('This is documentation for the unstable ' +
+                        'development version of Scikit-learn. (To use it, ' +
+                        '<a href="https://scikit-learn.org/stable/developers/advanced_installation.html#installing-nightly-builds">install the nightly build</a>.) ' + 
+                        'The latest stable ' +
+                        'release is <a href="https://scikit-learn.org">version ' +
+                        latestStable + '</a>.')
         }
     }
 })()
